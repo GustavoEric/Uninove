3a43dc2d06c9091799b5f6d05a02d57b30a47dcf
Gustavo Eric Alves Melo <gugaben28@gmail.com>
2023-10-19 09:12:40 -0300

    Atualização

--- Uninove_FrontEnd/Quiz/script.js
+++ Uninove_FrontEnd/Quiz/script.js
@@ -4,6 +4,8 @@ questao2 = document.getElementsByName('q2')
 questao3 = document.getElementsByName('q3')
 pontuacao = document.querySelector('#pontuacao')
 
+pontuacao.style.display = 'none'
+
 console.log(pontuacao)
 botao.addEventListener("click", function () {
     var pontos = 0;
@@ -16,6 +18,8 @@ botao.addEventListener("click", function () {
     if(questao3[3].checked){
         pontos+=4
     }
-    pontuacao.display = " "
+    pontuacao.style.display = ''
+    pontuacao.innerHTML = ''
+    pontuacao.innerHTML += '<p>Pontos</p>'
     pontuacao.innerHTML += '<p>'+pontos+'</p>'
 });
