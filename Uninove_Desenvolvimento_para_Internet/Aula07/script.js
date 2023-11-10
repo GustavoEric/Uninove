56b6ef7ef120ba9d11ff318612595ef2cb29ae4f
Gustavo Eric Alves Melo <gugaben28@gmail.com>
2023-11-10 00:31:42 -0300

    mudanças

--- Uninove_Desenvolvimento_para_Internet/Aula07/script.js
+++ Uninove_Desenvolvimento_para_Internet/Aula07/script.js
@@ -1,16 +1,43 @@
 selectUf = document.getElementById('selectUf')
-    fetch('cidades.json')
+selectCidade = document.getElementById('selectCidade')
+botaoEnviar = document.getElementById('btnEnviar')
+fetch('uf.json')
     .then(response => response.json())
         .then(estados =>{
-            form.innerHTML = ""
             estados.map(estado =>{
                 const optEstados = document.createElement("option")
                 optEstados.setAttribute('value', estado.uf);
-                optEstados.innerText = estado.uf
+                optEstados.setAttribute('name', "optEstado");
+                optEstados.innerText = estado.estado
                 selectUf.appendChild(optEstados)
                 
             })
     })
+
+selectUf.addEventListener("change", function () {
+    optEstado = selectUf.options[selectUf.selectedIndex].value;
+    console.log(optEstado)
+    selectCidade.disabled = false
+    buscarCidades(optEstado)
+
+})
+function buscarCidades(uf){
+    selectCidade.innerHTML= ""
+    fetch('cidades.json')
+    .then(response => response.json())
+        .then(cidades =>{
+            cidades.map(cidade =>{
+                if (cidade.uf == uf){
+                    const optCidades = document.createElement("option")
+                    optCidades.setAttribute('value', cidade.cidade);
+                    optCidades.setAttribute('name', "optCidade");
+                    optCidades.innerText = cidade.cidade
+                    selectCidade.appendChild(optCidades)
+                }
+                
+            })
+    })
+}
 addAluno("gustavo","guga1234","franco","sp")
 function addAluno(nome,senha,cidade,uf){
     ra = Math.floor(Math.random() * 9999999) + 100000;
