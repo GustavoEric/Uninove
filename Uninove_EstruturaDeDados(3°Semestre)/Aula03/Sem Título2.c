b672c00766ac99315e82be03e2b4563b01d7ef5e
Gustavo Eric <gugaben28@gmail.com>
2024-04-02 09:11:28 -0300

    arquivos

---
+++ Uninove_EstruturaDeDados(3°Semestre)/Aula03/Sem Título2.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main (){
+	
+	int qtde = 0;
+	
+	printf("Digite a Quantidade de numeros: ");
+	scanf("%d",&qtde);
+	printf("%d ",qtde);
+	
+	return 0;
+}
